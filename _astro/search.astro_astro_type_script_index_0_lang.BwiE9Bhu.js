297169f05c4b4d8ab6b3755e50679d4af5c4dd8e
Remy (PI Architect) <remy@peninsulainsider.com.au>
2026-06-01 22:57:16 +1000

    Revert "build: deploy v4 homepage preview [skip ci]"
    This reverts commit 7f8698b03bab9d2dbb01f26b08550c2fc5dc3636.

---
+++ _astro/search.astro_astro_type_script_index_0_lang.BwiE9Bhu.js
@@ -0,0 +1,1 @@
+import{t as e,s as r}from"./search-client.D7zQyQya.js";function n(){const a=window.__PI_SB;return!!(a&&a.url&&a.key)}window.PISearchRpc={get available(){return n()},search:r,typeahead:e};
